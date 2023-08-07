ed3e9606c503436948be782c059508edbbd4ab22
Gabriella Marusyak <slipuhinag@gmail.com>
2023-08-07 16:18:49 +0300

    Updated App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,13 @@ import { ContactList } from "./ContactList/ContactList";
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContactsThunk } from 'redux/operations';
-import { selectFilter, selectFilteredContacts } from 'redux/selectors';
+import { selectFilter } from 'redux/selectors';
 // import { selectContacts } from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
-  // const contacts = useSelector((state) => state.contacts.contacts.items);
-    // const filter = useSelector((state) => state.filter.filter);
-  // const contacts = useSelector(selectContacts);
   const isLoading = useSelector((state) => state.contacts.contacts.isLoading);
   const error = useSelector((state) => state.contacts.contacts.error);
-   const contactsFilteredByName = useSelector(selectFilteredContacts);
   const filter = useSelector(selectFilter);
 
   useEffect(() => {
@@ -29,22 +25,12 @@ export const App = () => {
       {isLoading && !error && <b>  Loading...</b>}
       {error && <p>{error.message}</p>}
 
-      <ContactForm 
-        // filteredContacts={contactsFilteredByName} 
-       />
-
+      <ContactForm />
       <h1>Contacts</h1>
-
       <Filter filter={filter} />
-
-      <ContactList 
-      // filteredContacts={contactsFilteredByName} 
-      />
-   
+      <ContactList />
     </div>
   );
 };
 
- // const filteredContacts = contacts.filter(
-  //   (contact) => contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
-  // ) 
+ 
